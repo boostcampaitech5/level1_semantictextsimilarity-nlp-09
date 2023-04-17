7d15411d8891bf5f49e1565607aea63592e9c9ce
jiwonii97 <wldnjs2920@gmail.com>
2023-04-17 09:12:51 +0000

    fix) args 수정 및 finish 함수 추가

--- run.py
+++ run.py
@@ -29,23 +29,30 @@ if __name__ == '__main__':
     inference_config.set_folder_dir(folder_name)
 
     ## config의 sweep 사용 여부에 따라 sweep config 설정 판단
-    if not train_config.sweep:        
-        base_train(train_config, folder_name)
-    else:
-        # sweep_config['metric'] = {'name':'val_pearson', 'goal':'maximize'}  # pearson 점수가 최대화가 되는 방향으로 학습을 진행합니다. (미션2)
-        # sweep_config = { 
-        #     "method" : "random",
-        #     "metric": {
-        #         "goal": "minimize", 
-        #         "name": "val_loss"
-        #     },
-        #     "parameters" : {
-        #         "batch_size": {"values": [8, 16, 32]}
-        #     }
-        # }
-
-        with open(WANDB.CONFIG_PATH) as file:
-            sweep_config = yaml.safe_load(file)
+    if not train_config.sweep:
+        folder_name = make_log_dirs(CONFIG.LOGDIR_NAME)
+        
+        # config에 my_log 폴더 경로 기록
+        train_config.set_folder_dir(folder_name)
+        inference_config.set_folder_dir(folder_name)
+        
+        wandb_logger, sweep_config = sweep_main(folder_name)
+        base_train(train_config, sweep_config, wandb_logger)
+        base_inference(inference_config)
+        
+        wandb.finish()
+#     else:
+#         ## sweep_config['metric'] = {'name':'val_pearson', 'goal':'maximize'}  # pearson 점수가 최대화가 되는 방향으로 학습을 진행합니다. (미션2)
+#         # sweep_config = { 
+#         #     "method" : "random",
+#         #     "metric": {
+#         #         "goal": "minimize", 
+#         #         "name": "val_loss"
+#         #     },
+#         #     "parameters" : {
+#         #         "batch_size": {"values": [8, 16, 32]}
+#         #     }
+#         # }
             
         sweep_id = wandb.sweep(
             sweep=sweep_config,         # config 딕셔너리를 추가합니다.
