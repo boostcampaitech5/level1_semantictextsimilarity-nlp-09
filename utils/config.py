794ef1afa39f76188d06d0967ec3179ddfd57156
Jiwonii97 <wldnjs3633@naver.com>
2023-04-17 15:28:54 +0900

    feature) omegaconf 적용

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,7 @@
 import yaml
-import wandb
+from omegaconf import OmegaConf
+from constants import CONFIG
+
 
 class Config():
     def __init__(self, cfg, opt):
@@ -8,24 +10,23 @@ class Config():
         self.shuffle = bool(cfg[opt]["shuffle"])
         self.learning_rate = float(cfg[opt]["learning_rate"])
         self.sweep = bool(cfg[opt]["sweep"])
-        
+
         self.model_name = cfg["model"]["name"]
         self.train_path = cfg["model"]["train_path"]
         self.dev_path = cfg["model"]["dev_path"]
         self.test_path = cfg["model"]["test_path"]
         self.predict_path = cfg["model"]["predict_path"]
-        
+
     def set_folder_dir(self, folder_dir):
         self.folder_dir = folder_dir
-        
+
+
 def load_config(config_file):
     with open(config_file) as file:
         config = yaml.safe_load(file)
 
     return Config(config, "train"), Config(config, "inference")
 
-def load_wandb_config(config_file):
-    with open(config_file) as file:
-        config = yaml.safe_load(file)
 
-    wandb.config.update(config)
+def load_omegaconf():
+    return OmegaConf.load(CONFIG.CONFIG_PATH)
