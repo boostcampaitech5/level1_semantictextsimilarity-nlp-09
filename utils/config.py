1b24c03f58bf89e138956dc1381bed5d506a77a7
Jiwonii97 <wldnjs3633@naver.com>
2023-04-17 16:00:10 +0900

    fix) train과정에서 predict 데이터에 dev데이터가 들어갈 수 있도록 구분

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,6 @@
 import yaml
 
+
 class Config():
     def __init__(self, cfg, opt):
         self.batch_size = int(cfg[opt]["batch_size"])
@@ -7,16 +8,17 @@ class Config():
         self.shuffle = bool(cfg[opt]["shuffle"])
         self.learning_rate = float(cfg[opt]["learning_rate"])
         self.sweep = bool(cfg[opt]["sweep"])
-        
-        self.model_name = cfg["model"]["name"]
-        self.train_path = cfg["model"]["train_path"]
-        self.dev_path = cfg["model"]["dev_path"]
-        self.test_path = cfg["model"]["test_path"]
-        self.predict_path = cfg["model"]["predict_path"]
-        
+
+        self.model_name = cfg[opt]["name"]
+        self.train_path = cfg[opt]["train_path"]
+        self.dev_path = cfg[opt]["dev_path"]
+        self.test_path = cfg[opt]["test_path"]
+        self.predict_path = cfg[opt]["predict_path"]
+
     def set_folder_dir(self, folder_dir):
         self.folder_dir = folder_dir
-        
+
+
 def load_config(config_file):
     with open(config_file) as file:
         config = yaml.safe_load(file)
