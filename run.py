6fe932d6f88a8e9729b481755deecbd04b575c15
Jiwonii97 <wldnjs3633@naver.com>
2023-04-17 15:31:40 +0900

    feature) omegaconf를 적용한 setup 부분 수정

--- run.py
+++ run.py
@@ -1,47 +1,47 @@
 import os
-import json
-import wandb
-
 from constants import CONFIG
 
 from train import base_train
 from inference import base_inference
 from utils.log import make_log_dirs
-from utils.config import load_config
+from utils.config import load_config, load_omegaconf
 from utils.wandb import *
 
 if __name__ == '__main__':
-    ## config/config.yaml에서 파라미터 정보를 가져옵니다.
-    train_config, inference_config = load_config(CONFIG.CONFIG_PATH)
-    
-    ## my_log 폴더를 생성하는 코드
+    # config/config.yaml에서 파라미터 정보를 가져옵니다.
+    # train_config, inference_config = load_config(CONFIG.CONFIG_PATH)
+    config = load_omegaconf()
+
+    # my_log 폴더를 생성하는 코드
     if not os.path.isdir(CONFIG.LOGDIR_PATH):
         os.mkdir(CONFIG.LOGDIR_NAME)
-        
-    ## config의 sweep 사용 여부에 따라 sweep config 설정 판단
-    if not train_config.sweep:
-        folder_name = make_log_dirs(CONFIG.LOGDIR_NAME)
-        
+
+    # config의 sweep 사용 여부에 따라 sweep config 설정 판단
+    if not config.train.sweep:
+        folder_name = make_log_dirs(CONFIG.LOGDIR_PATH)
+
         # config에 my_log 폴더 경로 기록
-        train_config.set_folder_dir(folder_name)
-        inference_config.set_folder_dir(folder_name)
-        
-        wandb_logger, sweep_config = sweep_main(folder_name)
-        base_train(train_config, sweep_config, wandb_logger)
-        base_inference(inference_config, sweep_config, wandb_logger)
+        config.folder_dir = folder_name
+
+        base_train(config)
+
+        # wandb_logger, sweep_config = sweep_main(folder_name)
+        # base_train(config, sweep_config, wandb_logger)
+        # base_inference(config, sweep_config, wandb_logger)
+
 #     else:
 #         ## sweep_config['metric'] = {'name':'val_pearson', 'goal':'maximize'}  # pearson 점수가 최대화가 되는 방향으로 학습을 진행합니다. (미션2)
-#         # sweep_config = { 
+#         # sweep_config = {
 #         #     "method" : "random",
 #         #     "metric": {
-#         #         "goal": "minimize", 
+#         #         "goal": "minimize",
 #         #         "name": "val_loss"
 #         #     },
 #         #     "parameters" : {
 #         #         "batch_size": {"values": [8, 16, 32]}
 #         #     }
 #         # }
-            
+
 #         sweep_id = wandb.sweep(
 #             sweep=sweep_config,     # config 딕셔너리를 추가합니다.
 #             project='boostcamp_STS'  # project의 이름을 추가합니다.
