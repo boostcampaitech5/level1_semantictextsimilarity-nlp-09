aa1932db1601aaafbb84b8e97778796ad25cfdca
T5144 <knlpscientist@gmail.com>
2023-04-20 03:11:44 +0000

    nn.Linear(self.plm.config.hidden_size, 1) 수정

--- model.py
+++ model.py
@@ -139,9 +139,10 @@ class Model(pl.LightningModule):
         self.plm = transformers.AutoModelForSequenceClassification.from_pretrained(
             pretrained_model_name_or_path=model_name, num_labels=1, hidden_dropout_prob=hidden_dropout_prob, attention_probs_dropout_prob=attention_probs_dropout_prob)
         self.num_layers = 1
-        # self.lstm = nn.LSTM(input_size=self.plm.config.hidden_size, hidden_size=self.plm.config.hidden_size, num_layers=self.num_layers, batch_first=True)
-        self.gru = nn.GRU(input_size=self.plm.config.hidden_size, hidden_size=self.plm.config.hidden_size, num_layers=self.num_layers, batch_first=True)
-        self.linear = nn.Linear(self.plm.config.hidden_size * 2, 1)
+        self.lstm = nn.LSTM(input_size=self.plm.config.hidden_size, hidden_size=self.plm.config.hidden_size, num_layers=self.num_layers, batch_first=True)
+        # self.gru = nn.GRU(input_size=self.plm.config.hidden_size, hidden_size=self.plm.config.hidden_size, num_layers=self.num_layers, batch_first=True)
+        # self.linear = nn.Linear(self.plm.config.hidden_size * 2, 1)
+        self.linear = nn.Linear(self.plm.config.hidden_size, 1) 
         # Loss 계산을 위해 사용될 MSELoss를 호출합니다.
         self.mse_loss_func = torch.nn.MSELoss()  # mse Loss 값
         self.mse_loss_l1 = torch.nn.L1Loss()  # L1 Loss 값
@@ -172,11 +173,10 @@ class Model(pl.LightningModule):
         outputs = self.plm(x, output_hidden_states=True)
         hidden_states = outputs['hidden_states'][-1]
 
-        gru_output, _ = self.gru(hidden_states)
-        cls_gru_output = gru_output[:, 0, :]
-        cls_gru_output = cls_gru_output.view(-1, self.plm.config.hidden_size)
+        lstm_output, _ = self.lstm(hidden_states)
+        cls_lstm_output = lstm_output[:, 0, :]
 
-        logits = self.linear(gru_output)
+        logits = self.linear(cls_lstm_output)
 
         return logits
 
