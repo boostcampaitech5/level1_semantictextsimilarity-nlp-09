d3be6d4415619dbdfb727da7cf7ef693ee6f2f7a
broco98 <qkrgydus--@naver.com>
2023-04-20 09:54:08 +0000

    lstm+dropout augmentation

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ import torch
 import torchmetrics
 import pytorch_lightning as pl
 
-
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, inputs, targets=[]):
         self.inputs = inputs
@@ -113,8 +112,17 @@ class Dataloader(pl.LightningDataModule):
     def predict_dataloader(self):
         return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
 
-
 class Model(pl.LightningModule):
+    
+    ## correlation loss function 함수
+    def correlation_loss_function(self, x, y):
+        x = x - torch.mean(x)
+        y = y - torch.mean(y)
+        x = x / (torch.sqrt(torch.sum(torch.square(x))) + 1e-5)
+        y = y / (torch.sqrt(torch.sum(torch.square(y))) + 1e-5)
+        corr = torch.mean(torch.mul(x, y))
+        return 1 - corr
+        
     def __init__(self, model_name, learning_rate, hidden_dropout_prob, attention_probs_dropout_prob):
         super().__init__()
         self.save_hyperparameters()
@@ -139,11 +147,16 @@ class Model(pl.LightningModule):
         self.linear = torch.nn.Linear(self.plm.config.hidden_size, 1)
         # https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html#torch.nn.LSTM 참고
         # num_layers로 layer수를 지정해 주세요!
-        self.lstm = torch.nn.LSTM(self.plm.config.hidden_size, self.plm.config.hidden_size, num_layers=1, bidirectional=False, batch_first=True)
+        self.lstm = torch.nn.LSTM(self.plm.config.hidden_size, self.plm.config.hidden_size, num_layers=2, bidirectional=False, batch_first=True)
+        
+        self.dropout = torch.nn.Dropout(p=0.1)
         
         # Loss 계산을 위해 사용될 MSELoss를 호출합니다.
         self.mse_loss_func = torch.nn.MSELoss()  # mse Loss 값
         self.mse_loss_l1 = torch.nn.L1Loss()  # L1 Loss 값
+        
+        ## loss 추가
+        # self.correlation_loss_function = correlation_loss_function()
 
     def forward(self, x):
         ## 마지막에 linear 모델 추가
@@ -160,8 +173,23 @@ class Model(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        logits = self(x)
-        loss = self.mse_loss_func(logits, y.float())
+        plm_outputs = self.plm(x)
+        plm_logits = plm_outputs.logits # [CLS] embedding vector를 반환
+        origin_last_hidden_state = plm_outputs.hidden_states[-1]
+        
+        ## dropout augmentation 적용
+        dropout_last_hidden_state = self.dropout(origin_last_hidden_state)
+        last_hidden_state = torch.cat((origin_last_hidden_state, dropout_last_hidden_state), 0)
+        
+        lstm_outputs, _ = self.lstm(last_hidden_state)
+        lstm_last_hidden_state = lstm_outputs[:,-1,:]
+        logits = self.linear(lstm_last_hidden_state)
+        
+        y = torch.cat((y,y),0)
+        
+        mse_loss = self.mse_loss_func(logits, y.float())
+        correlation_loss = self.correlation_loss_function(logits, y.float())
+        loss = (0.6 * correlation_loss) + (0.4 * mse_loss)
         
         self.log("train_loss", loss)
 
@@ -170,7 +198,11 @@ class Model(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
-        loss = self.mse_loss_func(logits, y.float())
+        
+        mse_loss = self.mse_loss_func(logits, y.float())
+        correlation_loss = self.correlation_loss_function(logits, y.float())
+        loss = (0.7 * correlation_loss) + (0.3 * mse_loss)
+
         
         self.log("val_loss", loss)
         self.log("val_pearson", torchmetrics.functional.pearson_corrcoef(
